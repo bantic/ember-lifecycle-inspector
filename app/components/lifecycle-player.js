a2062ec11f7260375bc9724fe5f43c6754ae1737
Cory <cory.forsyth@gmail.com>
2018-09-30 16:38:30 -0400

    Disable play button when needed

--- app/components/lifecycle-player.js
+++ app/components/lifecycle-player.js
@@ -1,12 +1,17 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { reads } from '@ember/object/computed';
+import { computed }  from '@ember/object';
 
 export default Component.extend({
   lifecycle: service(),
   events: reads('lifecycle.events'),
   components: reads('lifecycle.components'),
   eventIndex: 0,
+
+  noMoreEvents: computed('eventIndex', 'events.length', function() {
+    return this.eventIndex >= this.events.length;
+  }),
 
   currentEvent: null,
 
